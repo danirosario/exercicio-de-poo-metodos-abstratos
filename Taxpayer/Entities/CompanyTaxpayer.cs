cdb60d996e43ca8c067c2043e8686ce60a16ce47
dani <rosario.dani01@gmail.com>
2026-03-10 21:45:18 -0300

    add CompanyTaxpayer class; refactor IndividualTaxpayer

---
+++ Taxpayer/Entities/CompanyTaxpayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxpayer.Entities
+{
+    internal class CompanyTaxpayer : Taxpayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public CompanyTaxpayer() { }
+
+        public CompanyTaxpayer(int numberOfEmployees, string name, double annualIncome) : base(name, annualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            double tax;
+            if (NumberOfEmployees > 10)
+            {
+                tax = AnnualIncome * 0.14;
+            }
+            else
+            {
+                tax = AnnualIncome * 0.16;
+            }
+            return tax;
+
+        }
+    }
+}
--- Taxpayer/Entities/IndividualTaxpayer.cs
+++ Taxpayer/Entities/IndividualTaxpayer.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Taxpayer.Entities.Enums;
+using Taxpayer.Entities;
 
 namespace Taxpayer.Entities
 {
-    internal class IndividualTaxpayer
+    internal class IndividualTaxpayer : Taxpayer
     {
+        public double HealthExpenditures { get; set; }
+
+        public IndividualTaxpayer() { }
+
+        public IndividualTaxpayer(double healthExpenditures, string name, double annualIncome) : base(name, annualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            double tax;
+            if (AnnualIncome < 20000.00)
+            {
+                tax = AnnualIncome * 0.15;
+            }
+            else
+            {
+                tax = AnnualIncome * 0.25;
+            }
+            tax -= HealthExpenditures * 0.5;
+            return tax < 0 ? 0 : tax;  // Ensure that tax cannot be negative and return 0 if it is 
+        }
     }
 }
